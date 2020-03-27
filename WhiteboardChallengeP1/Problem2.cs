3061418ef4a6e0064e8006cb22a6979b9e078b5e
Joshuabrotman <joshuabrotman@Gmail.com>
2020-03-27 11:47:45 -0500

    completed Problem 2.

--- WhiteboardChallengeP1/Problem2.cs
+++ WhiteboardChallengeP1/Problem2.cs
@@ -10,7 +10,11 @@ namespace WhiteboardChallengeP1
     {
         public void Begin()
         {
-
+            Console.WriteLine("2.Palindrome is a word, phrase, or sequence that reads the same backward as forward i.e.madam.Write code that takes a user input and checks to see if it is a Palindrome and reports the result. You must handle spaces. ");
+            Console.WriteLine("Please enter a palindrome (or not) utilizing strings, and I will tell you if it is a palindrome:");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput.SequenceEqual(userInput.Reverse()));
+            Console.ReadLine();
         }
     }
 }
